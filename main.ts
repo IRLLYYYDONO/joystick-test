0b377d30766935d552dfd25d4b3c688eeb1c5f40
MAK|MAS|MP <87887719+IRLLYYYDONO@users.noreply.github.com>
2022-04-28 23:38:39 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let x = 0
+let y = 0
+let x_con = 0
+let y_con = 0
+let cordinateX = 2
+let cordinateY = 2
+
+while (true){
+    basic.pause(1000)
+    led.plot(cordinateX, cordinateY)
+
+    x = pins.analogReadPin(AnalogPin.P1)
+    y = pins.analogReadPin(AnalogPin.P0)
+
+    x_con = pins.map(x, 0, 1023, 0, 200)
+    y_con = pins.map(y, 0, 1023, 0, 200)
+
+    if (x_con > 100){
+        led.unplot(cordinateX, cordinateY)
+        cordinateX += 0.5
+    }else if (y_con > 100){
+        led.unplot(cordinateX, cordinateY)
+        cordinateY += 0.5
+    }
+}
